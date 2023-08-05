74f718a8acb9c34dfa7182dc9a891056acb75b3a
Giovanni <pino_otto@yahoo.com>
2023-08-05 22:28:27 +0800

    added get handler

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,11 @@
 
 use std::net::SocketAddr;
 
-use axum::Router;
+use axum::{response::IntoResponse, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new();
+    let app = Router::new().route("/todos", get(todos_index));
 
     let address = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum::Server::bind(&address)
@@ -24,4 +24,8 @@ async fn main() {
 
     // to continue
     // to learn
+}
+
+async fn todos_index() -> impl IntoResponse {
+    "ciao"
 }
